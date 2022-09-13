e812cbd81196347b6e7ec84f710140cba6110ece
Unero <ramadhanbhg@gmail.com>
2022-09-13 08:32:39 +0700

    major changes

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/un-ro/go-say-hello
+module github.com/un-ro/go-say-hello/v2
 
 go 1.19
--- say_hello.go
+++ say_hello.go
@@ -3,3 +3,7 @@ package go_say_hello
 func SayHello(name string) string {
 	return "Hello" + name
 }
+
+func DoCalculate() int {
+	return 1 + 1
+}
